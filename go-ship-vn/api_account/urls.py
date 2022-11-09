35425db382d4ff2cd4f6c7280163fa9ce99df7ae
huyledut <huyledut@gmail.com>
2022-11-09 20:22:45 +0700

    add send otp

--- go-ship-vn/api_account/urls.py
+++ go-ship-vn/api_account/urls.py
@@ -2,12 +2,13 @@ from unicodedata import name
 from django.urls import path
 from rest_framework import routers
 from .views import RegisterViewSet
-from api_account.views import login_view
+from api_account.views import login_view,Account_OTP
 from django.views.decorators.csrf import csrf_exempt
 name_app = 'register'
 router = routers.SimpleRouter()
 router.register(r'register', RegisterViewSet, basename='register')
 urlpatterns = [
     path("login/", csrf_exempt(login_view), name="login"),
+    path("send-otp/",Account_OTP.as_view(), name="send-otp"),
 ]
 urlpatterns += router.urls
--- go-ship-vn/api_account/views.py
+++ go-ship-vn/api_account/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -8,11 +7,14 @@ from rest_framework import status
 from .serializers import AccountSerializer
 from api_account.models import Account
 from rest_framework_simplejwt.tokens import RefreshToken
+from twilio.rest import  Client
+from rest_framework.views import APIView 
 from rest_framework.decorators import (
     api_view,
     authentication_classes,
     permission_classes,
 )
+from rest_framework.permissions import AllowAny
 from django.db.models import Q
 from django.contrib.auth.hashers import check_password
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -64,3 +66,41 @@ def login_view(request):
             return Response(response)
 
     return Response({"details": "Invalid username/password"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Account_OTP(APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        try:
+            phone_number = request.data.get("phone_number")
+            otp = request.data.get("otp")
+            account_sid = 'AC69e64d1e3a974b090a6b8d4d2b2bc08d'
+            auth_token = 'd42b4486b2488ae41f7823b2819b7c89'
+            client = Client(account_sid, auth_token)
+
+            message = client.messages.create(
+                messaging_service_sid='MG1e5faf909efa83f4c72531147bd4e6d8',
+                body='GoShip: Mã xác thực OTP của bạn là ' + otp,
+                to=phone_number,
+            )
+            print(str(message) + "id" +str(message.sid))
+            if message.sid:
+                print("Send successful")
+                return Response(data={
+                    'status': 'Success',
+                    'phone_number': phone_number,
+                }, status=status.HTTP_200_OK)
+        
+            print("Send failed")
+            return Response(data={ 
+                'status': 'Failure',
+                'phone_number': phone_number,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(data={
+                'status': 'Phone number is invalid',
+                'except': str(e)
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
